107515740d2ceb5c33ecacd375495716ff219676
zheng_zh <zheng_zh@LAPTOP-EUDJ3G84>
2021-11-09 17:06:13 +0800

    添加项目文件。

---
+++ IdentityServer4Demo/Config.cs
@@ -0,0 +1,78 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer4Demo
+{
+    public class Config
+    {
+        // scopes define the resources in your system
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1")
+            };
+        }
+
+
+        //public static IEnumerable<ApiScope> GetApiScopes()
+        //{
+        //    return new List<ApiScope>
+        //    {
+        //        new ApiScope("api1"),
+              
+        //    };
+        //}
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            string var = GrantTypes.ClientCredentials.FirstOrDefault();
+            // client credentials client
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    //ClientName = "clientName",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    // AllowedGrantTypes = new List<string>()
+                    //{
+                    //    GrantTypes.ClientCredentials.FirstOrDefault(),//Resource Owner Password模式
+                    //},
+                    //RequireConsent = false,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    //RedirectUris           = { "http://localhost:5000/signin-oidc" },
+                    //PostLogoutRedirectUris = { "http://localhost:5000/signout-callback-oidc" },
+                    AllowedScopes =
+                    {
+                        //IdentityServerConstants.StandardScopes.OpenId,
+                        //IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    }
+                    //AccessTokenLifetime = 60
+                  
+                    
+                  
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServer4Demo/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer4Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddControllers();
+         
+            // Adds IdentityServer
+            services.AddIdentityServer()//x => x.IssuerUri = "http://slb.com:8080"
+                     .AddDeveloperSigningCredential()
+                     .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                     .AddInMemoryApiResources(Config.GetApiResources())
+                     .AddInMemoryClients(Config.GetClients());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // Adds IdentityServer
+            app.UseIdentityServer();
+
+        }
+    }
+}
